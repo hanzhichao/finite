8f389703355338b1b6080abe6a25cf3d84e31faa
hzc <zhichao_han@sina.cn>
2026-03-05 20:21:29 +0800

    feat:移除窗口模糊效果

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -15,13 +15,13 @@ pub fn run() {
     .setup(|app| {
       let window = app.get_webview_window("main").unwrap();
 
-      #[cfg(target_os = "macos")]
-      apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, Some(16.0))
-        .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
-
-      #[cfg(target_os = "windows")]
-      apply_blur(&window, Some((18, 18, 18, 125)))
-        .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
+      // #[cfg(target_os = "macos")]
+      // apply_vibrancy(&window, NSVisualEffectMaterial::HudWindow, None, Some(16.0))
+      //   .expect("Unsupported platform! 'apply_vibrancy' is only supported on macOS");
+      //
+      // #[cfg(target_os = "windows")]
+      // apply_blur(&window, Some((18, 18, 18, 125)))
+      //   .expect("Unsupported platform! 'apply_blur' is only supported on Windows");
 
       Ok(())
     })
